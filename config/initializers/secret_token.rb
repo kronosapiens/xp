c6697dfbcf94a2aee1f2b06d2b404ee2fa4fd80e
Daniel Kronovet <kronovet@gmail.com>
2014-03-25 13:38:36 -0400

    initialized rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xp::Application.config.secret_key_base = 'e2857cb621f9c4e7d015462cda3c10b37a8b7ac3480e4d1682eb50dd594aaee60058e8c3f18ede8c8aa10b982381915e288c2871f3398c0ff43ca4c4cb732bf1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xp::Application.config.session_store :cookie_store, key: '_xp_session'
