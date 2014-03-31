3a290336bbb7b5ca88ede605a44d8db639889abf
sts10 <sschlinkert@gmail.com>
2014-03-31 17:30:54 -0400

    wip

--- app/assets/javascripts/lesson_form.js
+++ app/assets/javascripts/lesson_form.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){ 
-  
+
+
+  $('input#student-role').on("change", function(){
+    console.log("clicked");
+    $('div#select-locations').hide();
+    $('div#select-times').hide();
+  });
+
+  $('input#teacher-role').on("change", function(){
+    // alert('ahahah');
+    $('div#select-locations').show(); // dispaly block isn't right 
+    $('div#select-times').show();
+  });
 });
