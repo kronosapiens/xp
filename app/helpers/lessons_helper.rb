98417f5691df39c45bb11e3fe00d2f6382c85be2
Daniel Kronovet <kronovet@gmail.com>
2014-03-31 21:19:39 -0400

    wrote some helpers for the lessons views

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,11 @@
 module LessonsHelper
+
+  def prepopulate(lesson, tag)
+    "selected" if lesson.tag_ids_to_array.include?(tag.id)
+  end
+
+  def generate_tag_option(tag)
+    "<option id='#{tag.id}'>#{tag.name}</option>"
+  end
+
 end
