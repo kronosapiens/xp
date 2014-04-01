0374d5a133cee515e0d77b4d4fb947055c7b0ad9
Daniel Kronovet <kronovet@gmail.com>
2014-04-01 16:40:37 -0400

    added login check and redirect for sign_up actions

--- app/controllers/user_lessons_controller.rb
+++ app/controllers/user_lessons_controller.rb
@@ -1,5 +1,26 @@
 class UserLessonsController < ApplicationController
+  before_action :login_required
 
-  
-  
-end
+  def create
+    @user_lesson = UserLesson.new(user_lesson_params)
+    @user_lesson.user_id = current_user.id
+    @lesson = Lesson.find(@user_lesson.lesson_id)
+
+    respond_to do |format|
+      if @user_lesson.save
+        format.html { redirect_to @lesson, notice: 'Sign Up Successful!' }
+        format.json { render action: 'show', status: :created, location: @lesson }
+      else
+        format.html { redirect_to @lesson, notice: 'Sign Up Failed. Keep trying! :(' }
+        format.json { render json: @lesson.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_lesson_params
+      params.require(:user_lesson).permit(:role, :lesson_id)
+    end
+
+  end
