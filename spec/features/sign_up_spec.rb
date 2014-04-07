f9137d56bc0b07e2f29b6601e66fe75c65851d7f
Msea <Msea0j@gmail.com>
2014-04-07 17:51:07 -0400

    wrote tests for the button to join course

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -7,7 +7,6 @@ describe "Sign Up" do
     
   end
 
-
 it 'does allow you to take course if you are logged in' do
     $rspec_user_id = @user.id
     visit('/')
@@ -28,5 +27,30 @@ it 'requires that you be logged in to take course' do
     expect(page).to have_content("You must be logged in")
 
   end
+
+it 'does not allow you to take course twice' do
+    $rspec_user_id = @user.id
+    visit('/')
+    within "#lesson_#{@lesson.id}" do
+      click_button "Take This!"
+    end
+    visit('/')
+    within "#lesson_#{@lesson.id}" do
+      click_button "Take This!"
+    end
+
+    expect(page).to have_content("Sign Up Failed")
+end
+
+end
+
+describe "Teach Lesson" do
+  before(:each) do
+    @user = FactoryGirl.create(:user) 
+    @lesson = FactoryGirl.create(:lesson)
+    
+  end
+
+
 
 end
