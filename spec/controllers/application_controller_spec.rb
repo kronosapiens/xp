40a6050c88f63b97f6baddd0f0583179915e05a8
Daniel Kronovet <kronovet@gmail.com>
2014-04-21 13:49:42 -0400

    started writing controller tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 
 describe ApplicationController do
+  let!(:user){ create(:user) }
 
   before(:each) do
+    $rspec_user_id = user.id
+  end
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      get :show
+      expect(response).to be_success
+    end
+  end
+
+  it "returns nil for if there is no one logged in" do
+    @request.reset_session
+    expect(@controller.current_user).to eq(nil)
+  end
+
+  it "can return all tags with multiple scopes" do
     @lesson1 = create(:lesson)
     @lesson2 = create(:lesson)
     @lesson2.update(:status => "closed")
@@ -13,14 +30,7 @@ describe ApplicationController do
     @topic_tag1.lesson_tags.create(:lesson => @lesson1)
     @topic_tag1.lesson_tags.create(:lesson => @lesson2)
     @topic_tag2.lesson_tags.create(:lesson => @lesson2)
-  end
 
-  it "returns nil for if there is no one logged in" do
-    @request.reset_session
-    expect(@controller.current_user).to eq(nil)
-  end
-
-  it "can return all tags with multiple scopes" do
     @topic_tags = @controller.get_tags(:active)
     expect(@topic_tags).to include(@topic_tag1)
   end
--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -2,23 +2,93 @@ require 'spec_helper'
 
 describe LessonsController do
   let!(:user) { create(:user) }
+  let!(:lesson) { create(:lesson) }
 
   before(:each) do
     $rspec_user_id = user.id
   end
 
   describe "GET 'index'" do
-
     it "returns http success" do
       get :index
       expect(response).to be_success
+      expect(response.status).to eq(200)
     end
 
-    xit "has the xp logo on it" do
+    it "loads all active lessons" do
       get :index
-      expect(response).to include("xp")
+      assigns(:lessons).should be_true
     end
+  end
 
+  describe "GET 'show'" do
+    it "returns http success" do
+      get :show, id: lesson.id
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get :new
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "returns http success" do
+      lesson.registrations.create(user: user, admin: true)
+      get :edit, id: lesson.id
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "POST 'create'" do
+    it "returns http success" do
+      create(:tag)
+      post :create, lesson: { title: "Test Lesson",  description: "Test Description", references: "Test References" }, :tags => ["1"]
+      expect(response).to be_redirect
+      expect(response.status).to eq(302)
+    end
+  end
+
+  describe "PATCH 'update'" do
+    xit "returns http success" do
+      lesson.registrations.create(user: user, admin: true)
+      patch :update, id: lesson.id
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "PATCH 'update_status'" do
+    xit "returns http success" do
+      lesson.registrations.create(user: user, admin: true)
+      patch :update_status, id: lesson.id
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "DELETE 'delete'" do
+    xit "returns http success" do
+      lesson.registrations.create(user: user, admin: true)
+      delete :delete, id: lesson.id
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "POST 'admin_email'" do
+    xit "returns http success" do
+      lesson.registrations.create(user: user, admin: true)
+      post :admin_email, id: lesson.id
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
   end
 
 end
