e88786324c7fb8719cbb426147bfc8597f4454e7
Daniel Kronovet <kronovet@gmail.com>
2015-03-24 00:25:43 -0400

    removed requirement to be a flatiron student

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,18 @@ class SessionsController < ApplicationController
 
   def oauth
     auth = request.env["omniauth.auth"]
-    if flatiron_student?(auth["credentials"]["token"])
-      user = User.find_or_create_by_oauth(auth)
-      login(user)
-      redirect_to root_path, notice: "Successfully logged in. Welcome to xp!"
-    else
-      redirect_to root_path, alert: "Must be a Flatiron student to use xp... (for now)."
-    end
+    user = User.find_or_create_by_oauth(auth)
+    login(user)
+    redirect_to root_path, notice: "Successfully logged in. Welcome to xp!"
   end
+  
+    # if flatiron_student?(auth["credentials"]["token"])
+    #   user = User.find_or_create_by_oauth(auth)
+    #   login(user)
+    #   redirect_to root_path, notice: "Successfully logged in. Welcome to xp!"
+    # else
+    #   redirect_to root_path, alert: "Must be a Flatiron student to use xp... (for now)."
+    # end
 
   def destroy
     reset_session
